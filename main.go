48004ee0f331c21784dffb31ca7a839145c91f9b
jst-tsushima <index0205@gmail.com>
2024-09-02 08:08:01 +0900

    update: travel途中

--- main.go
+++ main.go
@@ -1,83 +1,48 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var n int
 	fmt.Scanf("%d", &n)
 
-	a := make([]int, n)
+	r := make([]bool, n)
+
 	for i := 0; i < n; i++ {
-		fmt.Scan((&a[i]))
-	}
+		var t, x, y int
+		fmt.Scanf("%d %d %d", &t, &x, &y)
 
-	counts := make([]int, n)
-	for i, v := range a {
-		counts[i] = 0
-		// 条件を満たしている間ループし続ける
-		for v%2 == 0 {
-			v /= 2
-			counts[i]++
+		// fmt.Printf("%d, %d, %d", t, x, y)
+
+		// 2の場合、y=-x+2, y=-x
+		// 3の場合、y=-x+3, y=-x+1
+		// 6の場合、y=-x+6, y=-x+4, y=-x+2, y=-x
+
+		for t >= 0 {
+			if y == -x+t {
+				r[i] = true
+				break
+			}
+			t -= 2
+		}
+		if t == -1 {
+			r[i] = false
 		}
 	}
 
-	// AtCoderで使われているGoのバージョン1.20.6だと存在しない？
-	// fmt.Println(slices.Max(counts))
-	// fmt.Println(counts)
+	// fmt.Println(r)
 
-	// 以下で代用
-	min := counts[0]
-	for _, v := range counts {
-		if min > v {
-			min = v
+	var j bool = true
+	for _, v := range r {
+		if !v {
+			j = false
 		}
 	}
-	fmt.Println(min)
 
-	// scanner := bufio.NewScanner(os.Stdin)
-	// // scanner.Scan()
-	// // s := scanner.Text()
+	if j {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
 
-	// // fmt.Println(strings.Count(s, "1"))
-
-	// var count int
-	// if scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	// 空白(スペースやタブ)を区切り文字として文字列を分割
-	// 	fields := strings.Fields(line)
-	// 	for _, field := range fields {
-	// 		for field % 2 != 0 {
-	// 			count++
-	// 		}
-	// 	}
-	// }
-
-	// num := nums[0]
-	// total := nums[1]
-
-	// for i := 0; i < num; i++ {
-	// 	for j := 0; j < num; j++ {
-	// 		k := num - i - j
-	// 		if 10000*i+5000*j+1000*k == total && k >= 0 {
-	// 			fmt.Printf("%d %d %d\n", i, j, k)
-	// 			return
-	// 		}
-	// 	}
-	// }
-	// fmt.Println("-1 -1 -1\n")
-
-	// i := []int{5, 3, 2, 8, 7}
-	// // s := []string{"d", "a", "f"}
-
-	// fmt.Println(i)
-	// // 昇順でソート
-	// sort.Ints(i)
-	// fmt.Println(i)
-
-	// // 降順でソート
-	// sort.Sort(sort.Reverse(sort.IntSlice(i)))
-	// fmt.Println(i)
-
+	}
 }
